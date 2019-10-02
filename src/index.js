8bc52766947e1133a24422beedc881d8df7c11be
PHitkovich <phitkovich@gmail.com>
2019-10-03 00:33:28 +0300

    feat: add implementation morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,35 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const NEW_MORSE_TABLE = {
+        '**********': " "
+    };
+    for (let prop in MORSE_TABLE) {
+        let x = [];
+        for (let char of prop) {
+            if (char == '.') {
+                x.push(10);    
+            } else {
+                x.push(11);
+            }
+        }
+        let keyLetter = x.join('').padStart(10, '0');
+        NEW_MORSE_TABLE[keyLetter] = MORSE_TABLE[prop];
+    }
+
+    if (expr.length%10 == 0) {
+        let arrDecodedLatter = [];
+        let exprLength = expr.length/10;
+        for (let i = 0; i < exprLength; i++) {
+            arrDecodedLatter.push(NEW_MORSE_TABLE[expr.slice(i*10, (i+1)*10)]);
+        }
+        return arrDecodedLatter.join('');
+
+    } else {
+        console.log('expression is wrong');
+        return 'expression is wrong';
+    }
+    
 }
 
 module.exports = {
